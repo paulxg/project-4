ffff6e885b0f599114c984a7844ca16d6315386c
paulxg <aaron3paul@gmail.com>
2026-06-13 13:39:43 +0200

    Update TODO.py

---
+++ ticket system 8/data/TODO.py
@@ -0,0 +1,38 @@
+#claim/view (tickets (also abarbeiten))
+#zeitbasierte Priorisierung von tickets
+#Priorisierung nach company/private
+#error handling: im Terminal oder auch im Programm?
+#todo ticket autolöschen nach ticketschließen; alternativ: prio=0
+#todo comment spalte wieder entfernen aus MySQL
+#automatisch Fenster close nach comment (admin) unnötig
+#todo chat anzeigen nach submit statusänderung (zu aufwändig)
+#todo mytickets responsible admin ID: Username hinzufügen
+#todo Ticketlöschen nach 30 Tagen im closed Zustand
+#todo User bestätigt gelöstes Problem bevor finaler close des tickets
+#todo Fenster schöner machen
+#todo passwort hashen mit bcrypt (Paul)
+#todo user löschen/bearbeiten etc.
+#todo Wie kann der Admin mit dem Kunden in Kontakt treten? (Anstelle von Username vllt email?)
+#todo Filterfunktion (mit order/Group by sql befehl)
+#todo my account view / profil zum ändern von Name etc
+#todo anschauen von rank userinfo etc
+#todo created tickets resolved tickets open tickets (count)
+#todo Suchfeld in Ticket Tabelle zum Suchen nach stichwörtern
+#todo passwort ändern im Profil
+#todo admin dashboard (wv tickets sind offen/ in bearbeitung pro kategorie etc)
+#fehlerlog?
+#todo ticket nur bearbeiten/zurückziewhen wenn auch offen
+#todo alte tickets im log?
+#darkmode?
+#todo claim ticket/ bearbeiter definieren
+#todo tickets als pdf csv exportieren
+#todo log, wer hat wann was gelöscht
+#passwort stärke bei registrtierung
+#todo Datenbankverbindung auto wieder herstellen wenn weg ohne restart
+#todo Animierter Übergang zwischen Fenstern (vllt einfach stacked widget)
+#todo Farbige Kategorie-Badges in der Ticket-Tabelle
+#todo Ticket-Zähler als Badge auf dem „My Tickets"-Button
+#todo Ticket-Dringlichkeit automatisch aus der Beschreibung erkennen (Schlüsselwörter wie „dringend", „funktioniert nicht")
+#todo Dark/Light Mode pro Nutzer gespeichert in der Datenbank
+#todo filter nach insults
+#todo user notification bei Ticket Bearbeitung
